4f1ab60a86771bedead08f29456895201bd5022b
jgregorytweedie <71092974+jgregorytweedie@users.noreply.github.com>
2021-09-20 18:44:37 -0600

    updated FetchBody.js
    I added in some comments describing every aspect of the code. I also added in more properties for each body. Updated README for todos! and added in a concept art

--- README.md
+++ README.md
@@ -7,5 +7,8 @@
 1. Fetch API data and render it on page
 
 - My problem with fetching data is how constantly i've been bouncing between different techniques. Primarily i've been using React hooks to fetch data, however the documentation for React says that it's best practice to use class components with the ComponentDidMount life cycle. I think I'm going to try to stick to that, as the React hooks have been causing me way too many problems.
+- I fixed the problem and everything is rendering just how I want it so far! Next thing, I want to fetch only one object within an array
 
 2. Make it look nice and fancy
+
+- I have a proof of concept of how I want it all to look, roughly.
--- src/Components/FetchBody.js
+++ src/Components/FetchBody.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 
 class FetchBody extends Component {
-
+/* ok how this works: I have a state, this is where the information
+from the api will be stored. i have a state of "isLoaded" as well so I can tell
+react to render things conditionally. E.g. when the isLoaded is set to false, then bodies will render.
+*/
   constructor(props) {
     super(props);
     this.state = {
@@ -10,6 +13,11 @@ class FetchBody extends Component {
 
     }
   }
+  // Bodies is to be rendered as an array.
+// I'm using component didMount to fetch data. This loads AFTER the "render".
+// I fetch the data on l. 20, then l. 21 I take the response and convert it to json format
+// then within that promise, which .then is, I'm setting the state to contain the array within the json.
+// As well, the boolean will be true for isLoaded, which later in my code, I have an element rendered if the boolean is set to true.
 
   componentDidMount() {
     fetch("https://api.le-systeme-solaire.net/rest/bodies/")
@@ -24,12 +32,19 @@ class FetchBody extends Component {
       console.log(error)
     })
   }
-  
+// I also have a .catch promise to catch and console log any errors.
+
+/**
+ * JAYDEN REMEMBER: When you have an error, console log each step to see what 
+ * Problem is!!!
+ */
 
   render() {
-
+    // declaring a variable for the state boolean.
     const { isLoaded } = this.state
-
+    // if else chain for the boolean. Simply put: If the boolean for the state of IsLoaded is true,
+    // then the following div will render. But then, if it's not loading, and it has fetched the data,
+    // then it returns the intended elements.
     if (!isLoaded) {
       return <div>Still loading...</div>
     }
@@ -41,11 +56,21 @@ class FetchBody extends Component {
         <ul>
           {this.state.bodies.map(body =>(
             <li key="{id}">
-              {body.id } | {body.englishName}
+               {body.englishName}
+               {body.gravity} 
             </li>
-            
+            // fun fact, this "gravity" property measures in G's.
+           // In here, I have englishName which is one of the objects within the array. I'm going to implement other elements of the array too! 
           ))}
         </ul>
+        <div>
+          {this.state.bodies.map(body =>(
+            <div key="index">
+              {body.density}
+              {body.avgTemp}
+            </div>
+          ))}
+        </div>
       </div>
     )
     }
